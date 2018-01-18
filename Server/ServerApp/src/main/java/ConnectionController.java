7aaa41da89d034a2c70c7fe38c1575e0d83ad744
maxheroux <maxime.heroux.1@ens.etsmtl.ca>
2018-01-18 15:50:52 -0500

    Restful webservice not working on my laptop

---
+++ Server/ServerApp/src/main/java/ConnectionController.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConnectionController 
+{
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/Connection")
+	public Connection connection(@RequestParam(value="name", defaultValue="User") String name)
+	{
+		return new Connection(counter.incrementAndGet(), String.format(template, name));
+	}
+}
