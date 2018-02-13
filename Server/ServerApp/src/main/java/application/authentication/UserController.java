790afe554c5187d6c55830b7fb74b94f3e754ee7
maxheroux <maxime.heroux.1@ens.etsmtl.ca>
2018-02-13 14:29:08 -0500

    Finished token, and added Message. Ready to merge

--- Server/ServerApp/src/main/java/application/authentication/UserController.java
+++ Server/ServerApp/src/main/java/application/authentication/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.message.Message;
+import application.utilities.AuthenticationFunctions;
 import application.utilities.HashingFunctions;
 
 @RestController
@@ -19,44 +21,33 @@ public class UserController
 	private EntityManager entityManager;
 	
 	@RequestMapping("/connection")
-	public Token connection(@RequestParam(value="username", required = true) String username
+	public Message connection(@RequestParam(value="username", required = true) String username
 		, @RequestParam(value="password", required = true) String password)
 	{
 		User user = (User) entityManager.createQuery("from User where username = :username")
 				.setParameter("username", username)
 				.getSingleResult();
 		
-		String token = user.getToken();
+		String salt = user.getSalt();
+		String cryptedPassword = HashingFunctions.hashPassword(password, salt);
 		
-		// TODO: Check if token is valid, else generate new token
-		
-		if (token.equals("I_AM_A_TOKEN"))
+		if (user.getPassword().equals(cryptedPassword))
 		{
-			return new Token(token);
+			// Generate new token
+			String token = AuthenticationFunctions.generateToken();
+			user.setToken(token);
+			userRepository.save(user);
+			
+			return new Message("Token", token);
 		}
 		else
 		{
-			String salt = user.getSalt();
-			String cryptedPassword = HashingFunctions.hashPassword(password, salt);
-			
-			if (user.getPassword().equals(cryptedPassword))
-			{
-				// Generate new token
-				token = "My_New_Token";
-				user.setToken(token);
-				userRepository.save(user);
-				
-				return new Token(token);
-			}
-			else
-			{
-				return new Token("BAD_AUTHENTICATION");
-			}
+			return new Message("Token", "BAD_AUTHENTICATION");
 		}
 	}
 	
 	@RequestMapping("/register")
-	public Token register(@RequestParam(value="username", required = true) String username
+	public Message register(@RequestParam(value="username", required = true) String username
 		, @RequestParam(value="password", required = true) String password
 		, @RequestParam(value="publicIp", required = true) String publicIp
 		, @RequestParam(value="port", required = true) int port)
@@ -64,14 +55,14 @@ public class UserController
 		String salt = HashingFunctions.generateSalt();
 		String cryptedPassword = HashingFunctions.hashPassword(password, salt);
 		
-		// Create methode that generates token
-		String token = "I_AM_A_TOKEN";
+		// Generate token
+		String token = AuthenticationFunctions.generateToken();
 		
 		// Store info in database
 		User user = new User(username, cryptedPassword, salt, publicIp, port);
 		user.setToken(token);
 		userRepository.save(user);
 		
-		return new Token(user.getToken());
+		return new Message("Token", user.getToken());
 	}
 }
