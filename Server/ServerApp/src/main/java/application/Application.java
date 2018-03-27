b639f95d7f827b5e78f638c5311cfbc1b452d107
maxheroux <maxime.heroux.1@ens.etsmtl.ca>
2018-03-27 14:39:10 -0400

    Removed garbage

--- Server/ServerApp/src/main/java/application/Application.java
+++ Server/ServerApp/src/main/java/application/Application.java
@@ -17,18 +17,5 @@ public class Application
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(Application.class, args);
-		
-		StateRequest req = new StateRequest(1,"julien","2f58261f-a0f2-403f-9d7a-ccf202a962a7");
-		
-		Gson gson = new Gson();
-		
-		gson.toJson(req, StateRequest.class);
-		
-		System.out.println(gson.toJson(req, StateRequest.class).toString());
-		Map<String,String> value = new HashMap<>();
-		value.put("RequestedTemperature", "22");		
-		StateChange change = new StateChange(1, "julien", "requestedTemperature", value, "asdasddas");
-		
-		System.out.println(gson.toJson(change, StateChange.class).toString());
 	}
 }
