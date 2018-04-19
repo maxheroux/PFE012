d00785ac16e2cf7dc8c8a167796aa5f40e4b038f
jgclement91 <julien-g.clement.1@ens.etsmtl.ca>
2018-04-19 15:20:28 -0400

    changed home_id to domicile_id in query

--- Server/ServerApp/src/main/java/application/repositories/DomicileRepository.java
+++ Server/ServerApp/src/main/java/application/repositories/DomicileRepository.java
@@ -11,7 +11,7 @@ import application.model.Domicile;
 
 public interface DomicileRepository extends CrudRepository<Domicile, Long> {
 
-	@Query(value = "Select * FROM client d INNER JOIN client u ON u.home_id = d.id WHERE u.id =:user_id ",nativeQuery=true)
+	@Query(value = "Select * FROM client d INNER JOIN client u ON u.domicile_id = d.id WHERE u.id =:user_id ",nativeQuery=true)
 
 	public Domicile findByUserId(@Param("user_id")int userId);
 	
