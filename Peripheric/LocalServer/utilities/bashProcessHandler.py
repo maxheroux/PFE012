d4c5255175e896350aadeba8d817bcac350de515
Julien <julien-g.clement.1@ens.etsmtl.ca>
2018-04-20 11:01:22 -0400

    release/bind all rfcomm devices (application startup)

--- Peripheric/LocalServer/utilities/bashProcessHandler.py
+++ Peripheric/LocalServer/utilities/bashProcessHandler.py
@@ -1,10 +1,20 @@
 import subprocess
-
+from dataAccess.peripheral import Peripheral_DAO
 
 class PeripheralCreationProcess:
 
     @staticmethod
+    def bind_peripherals():
+        subprocess.check_output(["utilities/rfcommReleaseScript"])
+        peripherals = Peripheral_DAO().get_peripherals()
+        for peripheral in peripherals:
+            rfcomm_channel = peripheral.rfcomm_device.replace('/dev/rfcomm', '')
+            subprocess.check_output(["utilities/rfcommBindScript", rfcomm_channel, peripheral.bluetooth_id])
+
+    @staticmethod
     def create_peripheral(bluetooth_id):
+
+        #TODO: Execute script on another thread
 
         new_rfcomm_channel = 0
         found_available_channel = False
@@ -25,6 +35,7 @@ class PeripheralCreationProcess:
 
             print("Creation of {} successful with /dev/rfcomm{}".format(bluetooth_id, new_rfcomm_channel))
             return "/dev/rfcomm{}".format(new_rfcomm_channel)
+
 
     @staticmethod
     def rfcomm_exists(channel):
